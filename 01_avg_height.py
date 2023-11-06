9c4aa4218e5923ce7baf912fa2293a9ec0ccb724
Rounak Mishra <133516852+rounakm535@users.noreply.github.com>
2023-11-06 22:14:41 +0530

    Add files via upload

--- 01_avg_height.py
+++ 01_avg_height.py
@@ -1,21 +1,18 @@
 # Input a Python list of student heights
-student_heights = input().split()
+
+student_heights = input("The height of students").split()
 for n in range(0, len(student_heights)):
   student_heights[n] = int(student_heights[n])
-# 🚨 Don't change the code above 👆
-  
-# Write your code below this row 👇
-
+# Your code below this row 👇
 total_height = 0
-for height in student_heights :
+for height in student_heights:
   total_height += height
-print(f"Total Height = {total_height}cm")
+print(f"total height = {total_height}")
 
-
-number_of_student = 0 
+number_of_students = 0
 for student in student_heights:
-  number_of_student += 1
-print(f"Number of Student = {number_of_student}")
+  number_of_students += 1
+print(f"number of students = {number_of_students}")
 
-avg_height = round(total_height/number_of_student)
-print(f"Avrage height = {avg_height}")
+average_height = round(total_height / number_of_students)
+print(f"average height = {average_height}")
