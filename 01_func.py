1c6075d344dbcd81ba5e3d49916818268bccb70e
Rounak Mishra <133516852+rounakm535@users.noreply.github.com>
2024-03-06 00:11:10 +0530

    Add files via upload

---
+++ 01_func.py
@@ -0,0 +1,8 @@
+def format_name(f_name, l_name):
+    for_f_name = (f_name.title())
+    for_l_name = (l_name.title())
+    return f"{for_f_name} {for_l_name}"
+
+
+new_string= format_name("rouank", "mishra")
+print(new_string)
