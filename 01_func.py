b0637430e9db4ec5108da8b52979280455c2f5f1
Rounak Mishra <133516852+rounakm535@users.noreply.github.com>
2024-03-13 23:08:48 +0530

    Add files via upload

--- 01_func.py
+++ 01_func.py
@@ -1,4 +1,5 @@
 def format_name(f_name, l_name):
+    """Take a first and last name and format it to return the title case version of the name."""
     for_f_name = (f_name.title())
     for_l_name = (l_name.title())
     return f"{for_f_name} {for_l_name}"
@@ -6,3 +7,4 @@ def format_name(f_name, l_name):
 
 new_string= format_name("rouank", "mishra")
 print(new_string)
+
